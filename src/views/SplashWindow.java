395fb23b482df6c5ae419a87e9c58c5e5128e8d7
DinukaDilshan415 <dinukadilshan8026@gmail.com>
2024-10-04 00:46:16 +0530

    Themes Package, views.Dashboard

--- src/views/SplashWindow.java
+++ src/views/SplashWindow.java
@@ -4,6 +4,9 @@
  */
 package views;
 
+import com.formdev.flatlaf.IntelliJTheme;
+import java.awt.Toolkit;
+
 /**
  *
  * @author kavindu
@@ -17,6 +20,7 @@ public class SplashWindow extends javax.swing.JFrame {
         initComponents();
         lodingAnimation();
         jLabel1.setIcon(new javax.swing.ImageIcon("src/resources/loading-animation2.gif"));
+        this.setIconImage(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/resources/icon2.png")));
     }
 
     //Progress Bar Code
@@ -98,30 +102,11 @@ public class SplashWindow extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SplashWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SplashWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SplashWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SplashWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+        
+        IntelliJTheme.setup(SplashWindow.class.getResourceAsStream(
+                    "/themes/arc-theme.theme.json"));
 
-        /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new SplashWindow().setVisible(true);
