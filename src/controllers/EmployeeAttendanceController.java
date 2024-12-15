dc91de013ba59646ab1aed42d3e68f8db2069a50
Kavindu Udara <155027870+kavindu-udara@users.noreply.github.com>
2024-12-15 21:53:13 +0530

    add date range selectors

--- src/controllers/EmployeeAttendanceController.java
+++ src/controllers/EmployeeAttendanceController.java
@@ -44,6 +44,10 @@ public class EmployeeAttendanceController {
         return MySqlConnection.executeSearch("SELECT * FROM `" + tableName + "` WHERE `employee_id`='" + employeeId + "' ");
     }
 
+    public ResultSet showByCustomQuery(String query) throws Exception {
+        return MySqlConnection.executeSearch(query);
+    }
+    
     public ResultSet storeWithNulls(EmployeeAttendance employeeAttendanceModel) throws Exception {
         return MySqlConnection.executeIUD("INSERT INTO `" + tableName + "`(`employee_id`, `attendance_date_id`, `attendance_status_id`) VALUES "
                 + "('" + employeeAttendanceModel.getEmployeeId() + "', "
